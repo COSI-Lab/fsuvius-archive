394f361c5e5c87af550d643810dcfb8ea96895cd
beadleha <beadleha@clarkson.edu>
2015-10-13 14:50:10 -0400

    Added default to 1 fsu, fixed grammar

--- fsucli.py
+++ fsucli.py
@@ -6,10 +6,13 @@ from pprint import pprint
 
 # Fsuvius CLI
 # Made by Benjamin Lannon
+# Minor tweaks from Alan Beadle
 # Commands:
 # python fsucli.py -> lists all accounts.
 # python fsucli.py dock <id> <num> -> docks <num> fsu from <id>.
+# python fsucli.py dock <id> -> docks 1 fsu from <id>.
 # python fsucli.py add <id> <num> -> adds <num> fsu to <id>.
+# python fsucli.py add <id> -> adds 1 fsu to <id>.
 
 URL="http://fsuvius.cslabs.clarkson.edu"
 
@@ -23,7 +26,7 @@ def list_accounts():
 
 def dock_fsu(aid, num):
     urlopen(URL + '/mod', urlencode({'aid': aid, 'amt': num, 'dock': True}))
-    print("docked {} fsu to ID {}").format(num, aid)
+    print("docked {} fsu from ID {}").format(num, aid)
     list_accounts()
 
 def add_fsu(aid, num):
@@ -35,9 +38,13 @@ def do_thing():
     if len(argv) == 1:
         list_accounts()
     else:
+        if len(argv) == 3:
+            fsu=1
+        else:
+            fsu=argv[3]
         if argv[1] == "dock":
-            dock_fsu(argv[2], argv[3])
+            dock_fsu(argv[2], fsu)
         elif argv[1] == "add":
-            add_fsu(argv[2], argv[3])
+            add_fsu(argv[2], fsu)
 
 do_thing()
